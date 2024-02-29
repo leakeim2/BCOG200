cc3cf7a5efb52c9be66a306e545a95f41e0982df
Lea Keim <69920985+leakeim2@users.noreply.github.com>
2024-02-28 21:03:37 -0600

    Update ReviewScraper.py

--- ReviewScraper.py
+++ ReviewScraper.py
@@ -1,9 +1,22 @@
 #scrape multiple reviews from books --> can I somehow pick the longest reviews?
-#books = {{title: volumeId, author, rating, synopsis, pageCount}, {}}
+#books = [{'id': i, 'title': t, 'author': a, 'rating': r, 'synopsis': s, 'pageCount': p}, {}]
 
 API_KEY = 'AIzaSyBPRZp5uuPc2Sp0rpIQzZofSJDmGcWv7Q8'
-url = 'https://www.googleapis.com/books/v1/volumes/{volumeId}'
+url = f'https://www.googleapis.com/books/v1/volumes/'
 
 class ReviewScraper:
     def __init__(self,books):
-        self.Id = books[
+        self.books = books
+        self.params = {'key': API_KEY}
+        
+    def scrap(self):
+        for b in books:
+            response = requests.get(url+b['id'],params=params
+            if response.status_code == 200:
+                data = response.json()
+            
+            else:
+                print('Error:', response.status_code)
+
+            
+            
