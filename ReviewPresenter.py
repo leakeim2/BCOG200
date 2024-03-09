6a6d897fc59256b7bab4a0f6d0cd187ce3354533
Lea Keim <69920985+leakeim2@users.noreply.github.com>
2024-03-09 14:33:00 -0600

    Update ReviewPresenter.py

--- ReviewPresenter.py
+++ ReviewPresenter.py
@@ -1,9 +1,7 @@
-from ReviewScraper import reviews
-from BookSelector import books
 # give the reader those reviews from which they will pick a book based on the review and then they get the name of the book
 # books = {"Book#":{}}
 # reviews = {"Book#": [{review},{review}]
 
 
 class ReviewPresenter:
-    def __init__(self, reviews):
+    def __init__(self, books, reviews):
