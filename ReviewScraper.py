599ab0789e17ce1760d997403c3aa10ff2bb42fe
Lea Keim <69920985+leakeim2@users.noreply.github.com>
2024-03-09 14:26:30 -0600

    Update ReviewScraper.py

--- ReviewScraper.py
+++ ReviewScraper.py
@@ -1,9 +1,10 @@
 #scrape multiple reviews from books --> can I somehow pick the longest reviews?
-#books = [{'id': i, 'title': t, 'author': a, 'rating': r, 'synopsis': s, 'pageCount': p}, {}]
+#books = {"Book #": {'id': i, 'title': t, 'author': a, 'rating': r, 'synopsis': s, 'pageCount': p}, "Book #": {},...}
 
 API_KEY = 'AIzaSyBPRZp5uuPc2Sp0rpIQzZofSJDmGcWv7Q8'
 url = 'https://www.googleapis.com/books/v1/volumes/'
 
+#reviews = {"Book #": [{review},{other review},{other other review}...]}
 class ReviewScraper:
     def __init__(self,books):
         self.books = books
@@ -12,7 +13,8 @@ class ReviewScraper:
         
     def scrap(self):
         for b in books:
-            response = requests.get(url+b['id'],params=params
+            reviews[b] = []
+            response = requests.get(url+books[b['id']],params=params)
             if response.status_code == 200:
                 data = response.json()
                 review_data = data.get('volumeInfo',{}).get('reviews',[])
@@ -22,12 +24,11 @@ class ReviewScraper:
                         content = r.get('content','No review text')
                         reviewer = r.get('author',{}).get('displayName','Anonymous')
                         date = r.get('published','Unknown')
-                        reviews[b]={'Review': content, 'Reviewer':reviewer,'Date':date})
-                    if b not in reviews:
-                        reviews.append(b: "Perhaps sometimes the consensus is objective. There are no one star reviews. If a book has no passionate haters then the book is not worth reading but do as you will")
-            
+                        reviews[b].append({'Review': content, 'Reviewer': reviewer,'Date': date})
             else:
                 print('Error:', response.status_code)
+
+#rn its just a simple get all the reviews and put them in dictionary
 
             
             
