ff2669ca2c53b7236df94ac0c9ca0d0a24b1cf28
Lea Keim <69920985+leakeim2@users.noreply.github.com>
2024-03-25 21:39:03 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,29 @@
+import unittest
+from main import Main
+from Buttons import Buttons
+from BookSelector import BookSelector
+from ReviewScraper import ReviewScraper
+from ReviewPresenter import ReviewPresenter
+
+
+class Test(unittest.TestCase):
+  def testButtons(self):
+    print("Buttons Testing is Complete")
+
+  def testBookSelector(self):
+    print("Book Selector Testing is Complete")
+
+  def testReviewScraper(self):
+    print("Review Scraper Testing is Complete")
+
+  def testReviewPresenter(self):
+    print("Review Presenter Testing is Complete")
+
+  def testMain(self):
+    print("Main Testing is Complete")
+    
+
+if __name__ == "__main__":
+  unittest.main()
+  print("\nYay your code works;)")
 
