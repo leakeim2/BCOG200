c81506621245017c3f3026be18991f8e62d0705d
Lea Keim <69920985+leakeim2@users.noreply.github.com>
2024-03-29 13:54:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from ReviewPresenter import ReviewPresenter
 
 
 class Main:
-    def __init__(self,options=None,happy=None,back=None):
+    def __init__(self,test=False,options=None,happy=None,back=None):
         self.op = options
         self.hap = happy
         self.back = back
-        self.run()
+        if test==False:
+            self.run()
+        else: self.runTest()
 
     def run(self):
-        
         while True:
             print("Hello. I will help you find a book. This book may be well regarded, but the consenus can be wrong and there are brutal reviews to prove it. ",
                   "Lets get started.\n")
@@ -24,13 +25,12 @@ class Main:
             root.mainloop()
             genre = b.get_genre()
           
-            if self.op==None:
-                self.op = input("Would you like options of books to choose from based on reviews? Enter Y/N")
-                self.op = True if self.op.upper()=='Y' else False
+            self.op = input("Would you like options of books to choose from based on reviews? Enter Y/N")
+            self.op = True if self.op.upper()=='Y' else False
 
             print("Sounds good. I'll take a look in the back and get you something to read")
 
-            b = BookSelector(genre, options)
+            b = BookSelector(genre, self.op)
             books = b.books
             
             r = ReviewScraper(books)
@@ -38,19 +38,37 @@ class Main:
 
             stop = True
             while stop:
-                something_to_read = ReviewPresenter(books, chosen_reviews)
-                if self.hap==None:      
-                    self.hap = input("Are you happy with your book? (Y/N)")
+                something_to_read = ReviewPresenter(books, chosen_reviews)    
+                self.hap = input("Are you happy with your book? (Y/N)")
                 if self.hap.upper() == 'Y': break
                 else:
-                    if self.back==None:  
-                        self.back = input("Let's go back to the other options (K), or you can fully restart(Y)")
+                    self.back = input("Let's go back to the other options (K), or you can fully restart(Y)")
                     if self.back.upper() == 'Y':
                         stop=False
             else: continue
             break
     
         print("okay yay now go read.")
+
+    def runTest(): 
+        while True:
+            self.op = True if self.op.upper()=='Y' else False
+            
+            b = BookSelector(fiction, self.op)
+            books = b.books
+            
+            r = ReviewScraper(books)
+            chosen_reviews = r.chosen_reviews
+
+            stop = True
+            while stop:
+                something_to_read = ReviewPresenter(books, chosen_reviews)    
+                if self.hap.upper() == 'Y': break
+                else:
+                    if self.back.upper() == 'Y':
+                        stop=False
+            else: continue
+            break
 
 if __name__ == "__main__":
     m = Main()
