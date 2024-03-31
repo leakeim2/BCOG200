5e674ba33a5d9d71ec5f3deb72ce067079ccf428
Lea Keim <69920985+leakeim2@users.noreply.github.com>
2024-03-31 17:34:32 -0500

    Update Buttons.py

--- Buttons.py
+++ Buttons.py
@@ -6,8 +6,7 @@ class Buttons:
         self.root = root
         self.dict = genres
         self.genre = None
-        self.key_buttons = []
-        self.val_buttons = []  
+        self.buttons = [] 
         self.show_keys()
 
     def show_keys(self):
@@ -17,38 +16,14 @@ class Buttons:
             self.key_buttons.append(button)
 
     def on_key_click(self, key):
-        if key == "No Preference":
-            self.genre = None
-            print("Selected genre:", None)
-            self.root.destroy()
-        else:
-            self.genre = key
-            self.clear_buttons(self.key_buttons)
-            self.show_values()
-
-    def show_values(self):
-        values = self.dict[self.genre]
-        for val in values:
-            button = tk.Button(self.root, text=val, command=lambda v=val: self.on_val_click(v))
-            button.pack()
-            self.val_buttons.append(button)
-        back_button = tk.Button(self.root, text="Go Back", command=self.go_back)
-        back_button.pack()
-        self.val_buttons.append(back_button)
-
-    def on_val_click(self, val):
-        if val is not "No Preference":
-          self.genre = val
-        print("Selected genre:", self.genre)
+        self.genre = key
         self.root.destroy()
-
-    def go_back(self):
-        self.clear_buttons(self.val_buttons)
-        self.show_keys()
-
-    def clear_buttons(self, buttons):
-        for b in buttons:
-            b.pack_forget()
 
     def get_genre(self):
         return self.genre
+
+def createButtons:
+    root = tk.TK()
+    b = Buttons(root)
+    root.mainloop()
+    return b.get_genre()
