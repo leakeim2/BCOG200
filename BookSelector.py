e70e3ab8243debda12345ed3d2db856144e65475
Lea Keim <69920985+leakeim2@users.noreply.github.com>
2024-05-09 14:36:16 -0500

    Update BookSelector.py

--- BookSelector.py
+++ BookSelector.py
@@ -1,36 +1,34 @@
 import requests
 from genres import genres
+import pandas as pd
 
 class BookSelector:
     def __init__(self, genre, options=True):
-        self.url = "https://biblioreads.eu.org/list/show/"
         self.genre = genre
+        self.books_df = pd.read_csv(genres[self.genre]+'.csv',encoding='latin-1')
         self.numBooks = 5
         if not options: self.numBooks = 1
         self.books = {}
-        self.url += genres[self.genre]
         self.fetch(self.numBooks)
 
     def fetch(self,n):
+        self.books_df['rating']= pd.to_numeric(self.books_df['rating'],errors='coerce')
+        df = self.books_df.dropna(subset=['rating'])
+        df = df[df['rating']>= 4.00]
+        random_books = df.sample(n=n,replace=False)
         count = 1
-        while len(self.books)<n:
-            response = requests.get(self.url)
-            if response.status_code == 200:
-                data = response.json()
-                return data
-                for item in data:
-                    title = 
-                    author = 
-                    synopsis = 
-                    pageCount = 
-                    self.books[str(count)]={'id': id,'title': title,'author': author,'rating': rating,'synopsis':synopsis,'pageCount': pageCount}
-                    count+=1
-                    if len(self.books) == n:
-                        break
-                if 'nextPageToken' in data:
-                    self.params['pageToken'] = data['nextPageToken']
-                else: break
-            else:
-                print("Error:", response.status_code)
-                break
+        for i, row in random_books.iterrows():
+            title = row['title']
+            url = row['url']
+            author = row['author']
+            rating = row['rating']
+            pageCount = row['pageCount']
+            synopsis = row['synopsis']
+            self.books[str(count)]={'url': url,
+                                  'title': title,
+                                  'author': author,
+                                  'rating': rating,
+                                  'synopsis':synopsis,
+                                  'pageCount': pageCount}
+            count+=1
 
