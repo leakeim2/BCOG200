5fdb57361b4aa8c0f2f8945ae1ad3c55e8340295
Lea Keim <69920985+leakeim2@users.noreply.github.com>
2024-05-10 12:24:02 -0500

    Update ReviewPresenter.py

--- ReviewPresenter.py
+++ ReviewPresenter.py
@@ -1,8 +1,7 @@
-# give the reader those reviews from which they will pick a book based on the review and then they get the name of the book
+# give the reader those reviews from which they will pick a book based on the review and then they get the book information
 # books = {"1":{}}
 # reviews = {"1": [{review},{review}]
 # chosen_review = {"1": review}
-
 
 class ReviewPresenter:
     def __init__(self, books, chosen_reviews):
@@ -10,16 +9,22 @@ class ReviewPresenter:
         self.chosen_reviews = chosen_reviews 
         self.go_forth_and_conquer()
 
+    #go through each review and call display_review
+    #then display the review corresponding to the user's input
     def go_forth_and_conquer(self):
         for b in self.books:
             self.display_review(b)
-        x = input("\nPlease enter a number 1-5 for your book selection.")
+        if len(self.books)>1:
+            x = input("\nPlease enter a number 1-5 for your book selection.")
+        else: x = 1
         self.display_choice(str(x))
 
+    #show the review to the reader with a label
     def display_review(self,key):
         print("REVIEW",key+":")
         print(self.chosen_reviews[key],'\n')
 
+    #show all the book information
     def display_choice(self,key):
         print("\nHere is your Book...\n")
         book = self.books[key]
